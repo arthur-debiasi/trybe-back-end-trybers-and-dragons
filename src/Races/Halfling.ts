3c0e5d91054175090173196b729f8f3c3dfe0209
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-02-16 16:54:07 -0300

    feat: req 02

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,21 @@
+import Race from './Race';
+
+export default class Halfling extends Race {
+  public _maxLifePoints: number;
+  private static _instancesNumber = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 60;
+
+    Halfling._instancesNumber += 1;
+  }
+
+  override get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static override createdRacesInstances(): number {
+    return this._instancesNumber;
+  }
+}
--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,11 +1,10 @@
 abstract class Race {
   private _name: string;
   private _dexterity: number;
-  private static _instancesNumber = 0;
+
   constructor(name: string, dexterity:number) {
     this._name = name;
     this._dexterity = dexterity;
-    Race._instancesNumber += 1;
   }
 
   static createdRacesInstances():number {
